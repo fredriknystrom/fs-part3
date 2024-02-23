d4dcbb61a0d25d96fc4caaa13e336b361606a3c0
fredriknystrom <fredriknystroms@gmail.com>
2024-02-23 14:04:21 +0100

    render start

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(function (tokens, req, res) {
     return [
       tokens.method(req, res),
@@ -96,9 +98,9 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
 
 
